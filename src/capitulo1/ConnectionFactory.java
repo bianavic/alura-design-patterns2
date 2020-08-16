4bc644af4bff7c1582fee1d5f789851691e23c38
Fabiana Assami <fabiana.assami@wirecard.com>
2020-08-16 13:27:10 -0300

    inserir logica mudar banco

--- src/capitulo1/ConnectionFactory.java
+++ src/capitulo1/ConnectionFactory.java
@@ -8,8 +8,16 @@ public class ConnectionFactory {
 
     public Connection getConnection() {
         try {
-            Connection conexao =
-                    DriverManager.getConnection("jdbc:mysql://localhost:3306/banco", "usuario", "senha");
+            Connection conexao;
+
+            String tipoBanco = System.getenv("tipoBanco");
+            if ("mysql".equals(tipoBanco)) {
+                conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/banco", "usuario", "senha");
+
+            }
+            else {
+                conexao = DriverManager.getConnection("jdbc:postgres://localhost:3306/banco", "usuario", "senha");
+            }
 
             return conexao;
         } catch (SQLException e) {
